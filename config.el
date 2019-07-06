306ede2aed45e09bdab0b4ebcc327d88ae6f1952
Adomas Jatužis <adomixaszvers@gmail.com>
2019-07-06 21:21:22 +0300

    Set customizations file

--- config.el
+++ config.el
@@ -5,3 +5,7 @@
 (after! (ediff org)
   (add-hook 'ediff-prepare-buffer-hook
             (lambda () (when (eq major-mode 'org-mode) (outline-show-all)))))
+
+(setq custom-file (expand-file-name "custom.el" doom-private-dir))
+(when (file-readable-p custom-file)
+  (load custom-file))
---
+++ custom.el
@@ -0,0 +1,22 @@
+;;; ~/.config/doom/custom.el -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(confirm-kill-emacs nil)
+ '(display-line-numbers-type (quote visual))
+ '(safe-local-variable-values
+   (quote
+    ((intero-targets "accumulate:lib" "accumulate:test:test")
+     (intero-targets "twelve-days:lib" "twelve-days:test:test")
+     (flycheck-disabled-checkers quote
+                                 (emacs-lisp-checkdoc))
+     (projectile-project-compilation-cmd . "home-manager switch")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
