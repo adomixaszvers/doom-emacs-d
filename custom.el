84fcb40bac6d54283505c5b34e63455bd15ef092
Adomas Jatužis <adomixaszvers@gmail.com>
2019-10-05 22:22:10 +0300

    dracula color scheme

--- custom.el
+++ custom.el
@@ -5,6 +5,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (doom-dracula)))
+ '(custom-safe-themes
+   (quote
+    ("f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" default)))
  '(display-line-numbers-type (quote visual))
  '(safe-local-variable-values
    (quote
